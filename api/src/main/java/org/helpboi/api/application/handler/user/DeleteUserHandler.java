934852a89ee1726b651a761f2813893d1636acf5
Christian <christian.winkel@offerista.com>
2020-03-21 19:08:10 +0100

    * improve delete handler

--- api/src/main/java/org/helpboi/api/application/handler/user/DeleteUserHandler.java
+++ api/src/main/java/org/helpboi/api/application/handler/user/DeleteUserHandler.java
@@ -1,7 +1,5 @@
 package org.helpboi.api.application.handler.user;
 
-import java.util.Optional;
-
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.transaction.Transactional;
@@ -9,7 +7,7 @@ import javax.transaction.Transactional;
 import org.helpboi.api.application.CommandHandler;
 import org.helpboi.api.application.command.user.DeleteUser;
 import org.helpboi.api.application.persistence.UserRepository;
-import org.helpboi.api.domain.model.user.User;
+import org.helpboi.api.domain.exception.NotFoundException;
 
 @Singleton
 public class DeleteUserHandler implements CommandHandler<DeleteUser> {
@@ -20,11 +18,10 @@ public class DeleteUserHandler implements CommandHandler<DeleteUser> {
     @Override
     @Transactional
     public void handle(DeleteUser command) {
-        Optional<User> user = userRepository.findById(command.getId());
-        if (user.isPresent()) {
-        	command.resolve(null);	
-        } else {
-        	command.notFound();
-        }
+        Long userId = command.getId();
+        userRepository.findById(userId)
+                .orElseThrow(() -> new NotFoundException(String.format(
+                        "User id: %s not found", userId)));
+       command.resolve(null);
     }
 }
