58d99e4a4a142827a865ef176badf6071de76986
Christian <christian.winkel@offerista.com>
2020-03-21 18:12:43 +0100

    * improve handlers for get operations

--- api/src/main/java/org/helpboi/api/application/handler/organistaion/GetOrganisationHandler.java
+++ api/src/main/java/org/helpboi/api/application/handler/organistaion/GetOrganisationHandler.java
@@ -1,7 +1,5 @@
 package org.helpboi.api.application.handler.organistaion;
 
-import java.util.Optional;
-
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.transaction.Transactional;
@@ -9,6 +7,7 @@ import javax.transaction.Transactional;
 import org.helpboi.api.application.CommandHandler;
 import org.helpboi.api.application.command.organisation.GetOrganisation;
 import org.helpboi.api.application.persistence.OrganisationRepository;
+import org.helpboi.api.domain.exception.NotFoundException;
 import org.helpboi.api.domain.model.organisation.Organisation;
 
 @Singleton
@@ -20,12 +19,10 @@ public class GetOrganisationHandler implements CommandHandler<GetOrganisation> {
     @Override
     @Transactional
     public void handle(GetOrganisation command) {
-        Optional<Organisation> organisation = organisationRepository
-                .findById(command.getId());
-        if (organisation.isPresent()) {
-        	command.resolve(organisation.get());
-        } else {
-        	command.notFound();
-        }
+        Long organisationId = command.getId();
+        Organisation organisation = organisationRepository.findById(command.getId())
+                .orElseThrow(() -> new NotFoundException(String.format(
+                        "Organisation id: %s not found", organisationId)));
+        command.resolve(organisation);
     }
 }
--- api/src/main/java/org/helpboi/api/application/handler/user/GetUserHandler.java
+++ api/src/main/java/org/helpboi/api/application/handler/user/GetUserHandler.java
@@ -1,7 +1,5 @@
 package org.helpboi.api.application.handler.user;
 
-import java.util.Optional;
-
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.transaction.Transactional;
@@ -9,6 +7,7 @@ import javax.transaction.Transactional;
 import org.helpboi.api.application.CommandHandler;
 import org.helpboi.api.application.command.user.GetUser;
 import org.helpboi.api.application.persistence.UserRepository;
+import org.helpboi.api.domain.exception.NotFoundException;
 import org.helpboi.api.domain.model.user.User;
 
 @Singleton
@@ -20,11 +19,10 @@ public class GetUserHandler implements CommandHandler<GetUser> {
     @Override
     @Transactional
     public void handle(GetUser command) {
-        Optional<User> user = userRepository.findById(command.getId());
-        if (user.isPresent()) {
-        	command.resolve(user.get());
-        } else {
-        	command.notFound();
-        }
+        Long userId = command.getId();
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new NotFoundException(String.format(
+                        "User id: %s not found", userId)));
+        command.resolve(user);
     }
 }
