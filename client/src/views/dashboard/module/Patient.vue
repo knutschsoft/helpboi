8c8c99e4143c8b8db41b115b67a5a24eeb1a0fc6
Christian <christian.winkel@offerista.com>
2020-03-23 00:06:00 +0100

    * improve patient details

--- client/src/views/dashboard/module/Patient.vue
+++ client/src/views/dashboard/module/Patient.vue
@@ -15,6 +15,43 @@
                 {{ patient.lastname }}
             </v-toolbar-title>
             <v-spacer/>
+            <v-btn text color="secondary">
+                <v-icon left>mdi-note-text-outline</v-icon>
+                Eintrag hinzufügen
+            </v-btn>
+            <v-divider inset vertical/>
+            <v-menu open-on-hover>
+                <template v-slot:activator="{ on }">
+                    <v-btn text color="secondary" class="mx-2" v-on="on">
+                        <v-icon left>mdi-medical-bag</v-icon>
+                        Maßnahme hinzufügen
+                    </v-btn>
+                </template>
+                <v-list>
+                    <template v-for="actionType in actions">
+                        <v-list-item @click="createAction(actionType)" :key="actionType">
+                            <v-list-item-icon>
+                                <v-icon>mdi-plus</v-icon>
+                            </v-list-item-icon>
+                            <v-list-item-title>{{actionType}}</v-list-item-title>
+                        </v-list-item>
+                        <v-divider :key="actionType"/>
+                    </template>
+                    <v-list-item @click="createInfo()">
+                        <v-list-item-icon>
+                            <v-icon>mdi-plus</v-icon>
+                        </v-list-item-icon>
+                        <v-list-item-title>Info hinzufügen</v-list-item-title>
+                    </v-list-item>
+                    <v-divider/>
+                    <v-list-item @click="createTask()">
+                        <v-list-item-icon>
+                            <v-icon>mdi-plus</v-icon>
+                        </v-list-item-icon>
+                        <v-list-item-title>Aufgabe hinzufügen</v-list-item-title>
+                    </v-list-item>
+                </v-list>
+            </v-menu>
         </v-toolbar>
 
         <v-card class="elevation-12 mt-5">
@@ -49,68 +86,58 @@
             </v-banner>
 
             <v-card-text>
-                <h3 class="mb-4">Personendaten</h3>
-                <v-text-field :value="patient.firstname + ' ' + patient.lastname" filled label="Geschlecht" readonly/>
-                <v-text-field :value="getGender(patient)" filled label="Geschlecht" readonly/>
-                <v-text-field :value="patient.address + ' ' + patient.zipcode + ' ' + patient.city" filled
-                              label="Anschrift" readonly/>
-                <v-text-field :value="patient.dateOfBirth" filled label="Geburtsdatum" readonly/>
-                <v-text-field v-model="patient.phone" filled label="Telefon" readonly append-icon="mdi-phone-forward"/>
-                <v-text-field v-model="patient.email" filled label="Email" readonly append-icon="mdi-email-edit"/>
 
-                <h3 class="mb-4">Vorerkrankungen</h3>
-                <v-textarea outlined value="Darmkrebs, HIV"/>
+                <v-row>
+                    <v-col>
+                        <h3 class="mb-4">Personendaten</h3>
+                        <v-text-field :value="patient.firstname + ' ' + patient.lastname" filled label="Geschlecht"
+                                      readonly/>
+                        <v-text-field :value="getGender(patient)" filled label="Geschlecht" readonly/>
+                        <v-text-field :value="patient.address + ' ' + patient.zipcode + ' ' + patient.city" filled
+                                      label="Anschrift" readonly/>
+                        <v-text-field :value="patient.dateOfBirth" filled label="Geburtsdatum" readonly/>
+                        <v-text-field v-model="patient.phone" filled label="Telefon" readonly
+                                      append-icon="mdi-phone-forward"/>
+                        <v-text-field v-model="patient.email" filled label="Email" readonly
+                                      append-icon="mdi-email-edit"/>
+
+                        <h3 class="mb-4">Vorerkrankungen</h3>
+                        <v-textarea outlined value="Darmkrebs, HIV"/>
+                    </v-col>
+                    <v-col>
+                        <v-tabs show-arrows background-color="transparent">
+                            <v-tab>Verlauf</v-tab>
+                            <v-tab-item>
+                                <v-timeline dense>
+                                    <v-timeline-item
+                                        v-for="history in patient.histories"
+                                        :key="history.id"
+                                        color="grey"
+                                        small
+                                        fill-dot
+                                    >
+                                        <v-card color="grey lighten-3" elevation="0">
+                                            <v-row class="pa-3">
+                                                <v-col cols="4">
+                                                    <strong>{{ history.createdAt }}</strong>
+                                                </v-col>
+                                                <v-col>
+                                                    <strong>{{ getHistoryTypeLabel(history) }}</strong>
+                                                    <div class="caption">{{ history.content }}</div>
+                                                </v-col>
+                                            </v-row>
+                                        </v-card>
+                                    </v-timeline-item>
+                                </v-timeline>
+                            </v-tab-item>
+                            <v-tab>Fragenkatalog</v-tab>
+                            <v-tab-item>
+                            </v-tab-item>
+                        </v-tabs>
+                    </v-col>
+                </v-row>
             </v-card-text>
         </v-card>
-
-        <v-toolbar flat color="transparent" class="mt-8">
-            <v-spacer/>
-            <v-btn text color="secondary">
-                <v-icon left>mdi-note-text-outline</v-icon>
-                Eintrag hinzufügen
-            </v-btn>
-            <v-divider inset vertical />
-            <v-btn text color="secondary">
-                <v-icon left>mdi-medical-bag</v-icon>
-                Maßnahme hinzufügen
-            </v-btn>
-        </v-toolbar>
-
-        <v-tabs show-arrows background-color="transparent">
-            <v-tab>Verlauf</v-tab>
-            <v-tab-item>
-                <v-timeline>
-                    <v-timeline-item
-                        v-for="history in patient.histories"
-                        :key="history.id"
-                    >
-                        {{ getHistoryTypeLabel(history) }}
-                        {{ history.content }}
-                        {{ history.createdAt }}
-                    </v-timeline-item>
-                </v-timeline>
-                <v-btn
-                    v-for="actionType in actions"
-                    :key="actionType"
-                    @click="createAction(actionType)"
-                >
-                    {{ actionType }}
-                </v-btn>
-                <v-btn
-                    @click="createInfo()"
-                >
-                    Info hinzufügen
-                </v-btn>
-                <v-btn
-                    @click="createTask()"
-                >
-                    Aufgabe hinzufügen
-                </v-btn>
-            </v-tab-item>
-            <v-tab>Fragenkatalog</v-tab>
-            <v-tab-item>
-            </v-tab-item>
-        </v-tabs>
     </v-container>
 </template>
 
