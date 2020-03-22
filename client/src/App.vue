0cb85d33cf16d2dd640c05af298124ab8089585b
robertfausk <robertfreigang@gmx.de>
2020-03-22 23:48:29 +0100

    [UX] Display api error message instead of just error status code

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <router-view/>
+    <router-view />
 </template>
 
 <script>
@@ -12,14 +12,26 @@
                 return this.$store.getters['security/isAuthenticated'];
             },
         },
+        data: () => ({
+        }),
         created() {
-            axios.interceptors.response.use(undefined, (err) => {
-                return new Promise(() => {
-                    if (err.response.status === 401) {
-                        this.$router.push({path: "/login"})
+            axios.interceptors.response.use(
+                function (response) {
+                    // Any status code that lie within the range of 2xx cause this function to trigger
+                    // Do something with response data
+                    return response;
+                }, function (error) {
+                    let reason = error;
+                    if (error.response && error.response.data && error.response.data.message) {
+                        reason = error.response.data.message;
                     }
-                    throw err;
-                });
+                    // if (error.response.status === 401) {
+                        // that.$router.push({path: "/abmelden"})
+                        // } else if (err.response.status === 500) {
+                        // this.showError = true;
+                    // }
+
+                return Promise.reject(reason);
             });
         },
     };
