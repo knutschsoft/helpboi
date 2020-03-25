e498e320deb396f575f5fcca5ca31954d7f50496
Christian <christian.winkel@offerista.com>
2020-03-25 11:24:45 +0100

    * separated navigation

--- client/src/views/dashboard/Dashboard.vue
+++ client/src/views/dashboard/Dashboard.vue
@@ -1,141 +1,21 @@
 <template>
     <v-app>
-        <v-navigation-drawer
-                v-model="drawer"
-                :mini-variant="mini"
-                app
-                dark
-                color="primary"
-                permanent
-                :expand-on-hover="isExpandOnHover"
-        >
-            <template v-slot:prepend>
-                <v-list nav two-line class="my-1">
-                    <v-list-item>
-                        <v-list-item-avatar>
-                            <v-avatar size="42">
-                                <v-img :src="require('../../assets/logo.png')"></v-img>
-                            </v-avatar>
-                        </v-list-item-avatar>
-                        <v-list-item-content>
-                            <v-list-item-title class="headline">
-                                Helpboi
-                            </v-list-item-title>
-                            <v-list-item-subtitle>
-                            </v-list-item-subtitle>
-                        </v-list-item-content>
-                    </v-list-item>
-                    <v-divider></v-divider>
-                    <v-list-item to="/abmelden" two-line>
-                        <v-list-item-action>
-                            <v-icon>mdi-logout</v-icon>
-                        </v-list-item-action>
-                        <v-list-item-content>
-                            <v-list-item-title>{{ hello }}</v-list-item-title>
-                            <v-list-item-subtitle>Abmelden</v-list-item-subtitle>
-                        </v-list-item-content>
-                    </v-list-item>
-                    <v-divider></v-divider>
-                </v-list>
-            </template>
-            <v-list nav>
-                <v-list-item
-                    v-if="user.organisationId"
-                    link
-                    to="/"
-                >
-                    <v-list-item-action>
-                        <v-icon>mdi-account-box</v-icon>
-                    </v-list-item-action>
-                    <v-list-item-content>
-                        <v-list-item-title>Patientenkartei</v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
-                <v-list-item
-                    v-if="user.organisationId"
-                    link
-                    to="/aufgaben"
-                >
-                    <v-list-item-action>
-                        <v-icon>mdi-calendar-today</v-icon>
-                    </v-list-item-action>
-                    <v-list-item-content>
-                        <v-list-item-title>Aufgaben</v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
-                <v-list-item
-                    v-if="user.admin && user.organisationId"
-                    link
-                    to="/organisation"
-                >
-                    <v-list-item-action>
-                        <v-icon>mdi-bank</v-icon>
-                    </v-list-item-action>
-                    <v-list-item-content>
-                        <v-list-item-title>Organisation verwalten</v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
-                <v-list-item
-                    v-else-if="!user.organisationId"
-                    link
-                    to="/organisation/erstellen"
-                >
-                    <v-list-item-action>
-                        <v-icon>mdi-bank-plus</v-icon>
-                    </v-list-item-action>
-                    <v-list-item-content>
-                        <v-list-item-title>Organisation erstellen</v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
-            </v-list>
-            <template v-slot:append>
-                <v-divider></v-divider>
-                <v-list shaped>
-                    <v-list-item @click="mini = !mini">
-                        <v-list-item-icon>
-                            <v-icon v-if="!mini">mdi-chevron-double-left</v-icon>
-                            <v-icon v-if="mini">mdi-chevron-double-right</v-icon>
-                        </v-list-item-icon>
-                        <v-list-item-content>
-                            <v-list-item-subtitle>Sidebar verkleinern</v-list-item-subtitle>
-                        </v-list-item-content>
-                    </v-list-item>
-                </v-list>
-            </template>
-        </v-navigation-drawer>
-
+        <Navigation />
         <v-content class="grey lighten-3">
             <div class="ma-3">
-                <router-view></router-view>
+                <router-view />
             </div>
         </v-content>
-
     </v-app>
 </template>
 
 <script>
+    import Navigation from "../../components/Navigation";
+
     export default {
         name: 'Dashboard',
-
-        data: () => ({
-            drawer: true, // Display side menu by default
-            mini: false,
-            isExpandOnHover: false,
-            user: false,
-        }),
-        computed: {
-            isAuthenticated() {
-                return this.$store.getters['security/isAuthenticated'];
-            },
-            hello() {
-                return `Hallo ${this.user.firstname}!`;
-            },
-            currentUser() {
-                return this.$store.getters['security/currentUser'];
-            },
-        },
-        created() {
-            this.user = this.$store.getters['security/currentUser'];
-        },
+        components: {
+            Navigation
+        }
     };
 </script>
